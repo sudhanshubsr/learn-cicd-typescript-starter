a7668f5bab9edccfd189dcc6e240f3dd4e3d8362
sudhanshu sharma <sudhanshubsr.dev@gmail.com>
2025-06-12 05:06:54 -0700

    Add security check using ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,16 @@
-import js from "@eslint/js";
+import { default as js, default as pluginJs } from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginSecurity from "eslint-plugin-security";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
